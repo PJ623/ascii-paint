247dec96356a0e54799f3d507d7b0ee2bda31f4c
PJ623 <jardiolin.paolo@gmail.com>
2018-07-03 18:25:57 -0700

    implemented canvas resize functionality, slightly improved style

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+// Turn this into some sort of page manager object?
+
 let canvas = document.getElementById("canvas");
 let renderingCanvas = document.getElementById("rendering-canvas");
-
+let currentWidth = 48;
+let currentHeight = 24;
+let height;
 let can = new TextCanvas();
 
-function makeCanvas() {
-    can.build(48, 24);
+function makeCanvas(width, height) {
+    if (width > 0 && height > 0) {
+        can.build(width, height)
+    } else {
+        can.build(currentWidth, currentHeight);
+    }
     can.bind(canvas);
     can.get(0, 0).focus();
 }
@@ -18,12 +26,44 @@ function stringifyArt() {
     alert("ASCII art copied into clipboard.");
 }
 
+function checkDimension(num) {
+    if (Number.isNaN(num) || num < 0 || num % 1 != 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 makeCanvas();
 
 document.getElementById("clear-button").addEventListener("click", () => {
-    makeCanvas();
+    makeCanvas(currentWidth, currentHeight);
 });
 
 document.getElementById("stringify-button").addEventListener("click", () => {
     stringifyArt();
+});
+
+let modal = document.getElementById("modal");
+
+document.getElementById("set-size-button").addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+document.getElementById("set-size-submit-button").addEventListener("click", () => {
+    let width = Number(document.getElementById("width-input").value);
+    let height = Number(document.getElementById("height-input").value);
+
+    if (!checkDimension(width) || !checkDimension(height)) {
+        alert("Dimensions invalid.");
+    } else {
+        makeCanvas(width, height);
+        currentWidth = width;
+        currentHeight = height;
+        modal.style.display = "none";
+    }
+});
+
+document.getElementById("set-size-cancel-button").addEventListener("click", () => {
+    modal.style.display = "none";
 });
